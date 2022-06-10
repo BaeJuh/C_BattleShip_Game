e394017046c3253e528e70f57260e52ba068c40a
Juhwan <106052739+BaeJuh@users.noreply.github.com>
2022-06-10 17:03:42 +0900

    Update bsgm.cpp

--- bsgm.cpp
+++ bsgm.cpp
@@ -1,10 +1,3 @@
-
-		
-		if (MenuCode == 0) {
-			inGame();
-		}
-		else if (MenuCode == 1) {
-			Manual();
 #include <iostream>
 #include <Windows.h>
 #include <conio.h>
@@ -110,20 +103,38 @@ int Menu() {
 	}
 }
 
+// 게임 시작
 void inGame() {
 	system("cls");
 	srand(time(NULL));
 	int map_size = 0;
-	int map_x, map_y; 
 	
-	printf("맵의 크기를 입력해주세요. \n크기 : ");
-	scanf("%d", &map_size);
+	gotoxy(0, remark);
+	printf("3이상의 정수만 입력해주세요.");
+	gotoxy(32, 6);
+	printf("맵의 크기를 입력해주세요.");
+	gotoxy(39, 9);
+	printf("크기 : ");
 	
-	map_x = map_size; // 임시************************************************** 
-	map_y = map_size;
+	while(1) {
+		scanf("%d", &map_size);
+		if (map_size == 0 || map_size < 3) {
+			system("cls");
+			gotoxy(0, remark);
+			printf("3이상의 정수만 입력해주세요.");
+			gotoxy(32, 6);
+			printf("크기를 다시 입력해주세요.");
+			gotoxy(39, 9);
+			printf("크기 : ");
+			rewind(stdin);
+		}
+		else {
+			break;
+		}
+		
+	}
 	
 	int Map[map_size][map_size]; // 배 없음 = 0, 배 있음 = 1
-	
 	for (int i=0; i<map_size; i++) { // 2차월 배열 0으로 초기화 
 		for (int j=0; j<map_size; j++) {
 			Map[i][j] = 0;
@@ -135,7 +146,7 @@ void inGame() {
 	int ship_locate_x; // rand()%map_size;
 	int ship_locate_y; // rand()%map_size;
 	int ship_direct; // rand()%2; // 0이면 가로로 1이면 세로로 
-	int ship_count = 3; // 배의 개수
+	int ship_count = rand()%(map_size - 1) + 1; // 배의 개수 (랜덤) 
 	bool isShiphere = false; // 배가 겹치는 것을 제거하기 위한 플래그 
 	
 	for (int i=0; i<ship_count; i++) { // 맵에 배 넣기 
@@ -170,75 +181,145 @@ void inGame() {
 					Map[ship_locate_y + j][ship_locate_x] = 1;
 				}
 			}
-			cout << ship_locate_x << " " << ship_locate_y << " " << ship_direct << " " << ship_size << endl;
 		} else {
 			i--;
 		}
 	}
 	
-	printf("\n\n------------------------------\n");
-	for (int i=0; i<map_size; i++) { // 2차월 배열 0으로 초기화 
-		for (int j=0; j<map_size; j++) {
-			if (Map[i][j] == 0) {
-				printf("■");
-			}
-			else {
-				printf("□");
-			}
-		}
-		printf("\n");
-	}
-	printf("\n------------------------------\n");
-	
 	// 게임플레이 
-	
 	bool isShipAlive = true;
 	int flag = 0;
 	int x, y;
-	int BattleField[map_size][map_size];
+	char BattleField[map_size][map_size];
 	for (int i=0; i<map_size; i++) { 
 		for (int j=0; j<map_size; j++) {
-			BattleField[i][j] = 0;
+			BattleField[i][j] = '~';
 		}
 	}
-	
-	while(isShipAlive) {
+	int turn = 0; 
+	bool QUIT = false;
+	while(isShipAlive) { // 유저가 플레이하는 부분 
 		system("cls");
+		
+		turn ++; // 턴 세기 
+		gotoxy(0, remark-1);
+		printf(" O : 맞춤, X : 빗나감");
+		gotoxy(0, remark);
+		printf("정수만 입력해주세요. (0 입력 시 게임 종료, -1 입력 시 정답 보기)", map_size-2, map_size-2);
+		
 		flag = 0;
-		printf("turn start\n");
-		for (int i=0; i<map_size; i++) { 
+		gotoxy(34, 4);
+		printf("적함을 %d개 격침시키세요.", ship_count);
+		for (int i=0; i<map_size; i++) {
+			gotoxy(46-map_size, 7+i); // 맵이 화면 중앙에 나오도록함 
 			for (int j=0; j<map_size; j++) {
-				printf("%d ", BattleField[i][j]);
+				printf("%c ", BattleField[i][j]);
 			}
 			printf("\n");
 		}
-		printf("좌표를 입력하세요 : ");
-		scanf("%d %d", &x, &y);
+		
+		printf("\n\n\t\t\t\t    좌표를 입력하세요 : ");
+		gotoxy(40, 10+map_size);
+		printf("X 좌표 : ");
+		scanf("%d", &x);
+		
+		while(x<1 || x>map_size) {
+			if (x == 0) {
+				QUIT = true;
+				break; 
+			}
+			if (x == -1) { // 정답 보기 
+				for (int i=0; i<map_size; i++) {  
+					gotoxy(47+map_size, 7+i);
+					for (int j=0; j<map_size; j++) {
+						if (Map[i][j] == 0) {
+							printf("■");
+						}
+						else {
+							printf("□");
+						}
+					}
+					printf("\n");
+				}
+			}
+			gotoxy(26, 10+map_size);
+			printf("X 좌표 ( 1~%d 사이 숫자만 입력해주세요. ): ", map_size);
+			scanf("%d", &x);
+		}
+		if (QUIT) {
+			system("cls");
+			break;
+		}
+		gotoxy(40, 11+map_size);
+		printf("Y 좌표 : ");
+		scanf("%d", &y);
+		while(y<1 || y>map_size) {
+			if (y == 0) {
+				QUIT = true;
+				break; 
+			}
+			if (y == -1) { // 정답 보기 
+				for (int i=0; i<map_size; i++) { 
+					gotoxy(47+map_size, 7+i);
+					for (int j=0; j<map_size; j++) {
+						if (Map[i][j] == 0) {
+							printf("■");
+						}
+						else {
+							printf("□");
+						}
+					}
+					printf("\n");
+				}
+			}
+			gotoxy(26, 11+map_size);
+			printf("Y 좌표 ( 1~%d 사이 숫자만 입력해주세요. ): ", map_size);
+			scanf("%d", &y);
+		}
+		if (QUIT) {
+			system("cls");
+			break;
+		}
 		
 		y = y-1;
 		x = x-1;
 		
 		if (Map[y][x]) {
-			BattleField[y][x] = 1;
+			BattleField[y][x] = 'O';
 			Map[y][x] = 0;
 		} else {
-			BattleField[y][x] = 2;
+			BattleField[y][x] = 'X';
 		}
 		
-		for (int i=0; i<map_size; i++) { 
+		for (int i=0; i<map_size; i++) { // 맵 전체를 검사해서 남은 배가 있는지 확인 
 			for (int j=0; j<map_size; j++) {
 				if (Map[i][j] == 1) {
 					flag ++;
-					Sleep(100);
 				}
 			}
 		}
 		if (flag == 0) {
+			system("cls");
+			gotoxy(32, 7);
+			printf("!!!!!!!!!!VICTORY!!!!!!!!!!");
+			gotoxy(33, 8);
+			printf("%d발 만에 적함을 모두 격침!", turn);
+			for (int i=0; i<map_size; i++) { 
+				gotoxy(46-map_size, 10+i);
+				for (int j=0; j<map_size; j++) {
+					printf("~ ");
+				}
+					printf("\n");
+			}
 			isShipAlive = false;
 		}
 	}
 	
-	printf("게임 종료");
+	gotoxy(39, 5);
+	printf("[ 게임 종료 ]");
+	gotoxy(0, remark);
+	printf("SPACEBAR를 누르면 타이틀 화면으로 돌아갑니다.");
+	
 	
 	while(true) {
 		int key = KeyControl();
@@ -248,25 +329,21 @@ void inGame() {
 	}
 }
 
-void drawMap(int size) { // □ ■ 
-	string Map[size][size];
-	for (int i=0; i<size; i++) {
-		for (int j=0; j<size; j++) {
-			Map[i][j] = "■";
-		}
-	}
-}
-
+// 메뉴창 
 void Manual() {
 	system("cls");
-	gotoxy(38, 5);
+	gotoxy(40, 5);
 	printf("[ 조작법 ]");
-	gotoxy(26, 8);
+	gotoxy(29, 8);
 	printf("좌표를 입력하면 대포를 발사합니다.");
-	gotoxy(32, 11);
-	printf("좌표 입력 예시 : 7 7");
+	gotoxy(24, 11);
+	printf("X와 Y 좌표에 각각 정수를 입력해줘야 합니다.");
+	gotoxy(16, 14);
+	printf("좌표에 0을 입력하면 게임 종료, -1을 입력하면 답을 보여줍니다.");
+	gotoxy(30, 17);
+	printf("Made by 2018265133 Insole Hwang");
 	gotoxy(0, remark);
-	printf("SPACEBAR를 누르면 메인으로 돌아갑니다.");
+	printf("SPACEBAR를 누르면 타이틀 화면으로 돌아갑니다.");
 	
 	while(true) {
 		int key = KeyControl();
